2e8251aa21aefff4f479bb4deeed2c862b7af316
Daniel Chiwuzoh <chiwuzohdumebi@gmail.com>
2022-12-28 09:35:40 +0100

    test dev deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,39 +1,55 @@
 #!/bin/bash
-env="development"
+env="staging"
 project="pouchfi"
+package_version=$(node -p "require('./package.json').version")
+
 if [ ! $@ ]
 then
     echo "Need env staging or production"
     echo "exit 1"
-    return 1
+    exit 1
 fi
 # override the env base on command
 env="$@"
 echo "deploying to $env..."
 
-# run this once
-kubectl create configmap pouchfi-env --from-env-file=./.env.staging
-kubectl apply -f k8s/secret.yaml
-kubectl create -f k8s/staging/pv.yaml
+# set image name and tag
+image_tag=$dh_username/pouchfi:$package_version
 
-kubectl create -f k8s/staging/redis.yaml -f k8s/staging/postgres.yaml
+echo "building docker image"
+docker build -t $image_tag .
 
-pod_name=$(kubectl get po -l app=pouchfi-redis | grep pouchfi-backend | awk '{print $1}')
+echo "Pushing image to docker hub"
+docker push $image_tag
 
-# check whether redis server is ready or not
-while true; do
-    pong=$(kubectl exec -it $pod_name redis -- redis-cli ping)
-    if [[ "$pong" == *"PONG"* ]]; then
-        echo ok;
-        break
-    fi
-done
+echo "Updating deployment file"
+sed -i 's|thedumebi/pouchfi:latest|'$image_tag'|gi' ./k8s/staging/deployment.yaml
 
-if [[ $env === 'production']]; then
-    kubectl create -f k8s/production/deployment.yaml
-else
-    kubectl create -f k8s/staging/deployment.yaml
-fi
+cat ./k8s/staging/deployment.yaml
 
-echo "done"
-echo "exit 0"
+# # run this once
+# kubectl create configmap pouchfi-env --from-env-file=./.env.staging
+# kubectl apply -f k8s/secret.yaml
+# kubectl create -f k8s/staging/pv.yaml
+
+# kubectl create -f k8s/staging/redis.yaml -f k8s/staging/postgres.yaml
+
+# pod_name=$(kubectl get po -l app=pouchfi-redis | grep pouchfi-backend | awk '{print $1}')
+
+# # check whether redis server is ready or not
+# while true; do
+#     pong=$(kubectl exec -it $pod_name redis -- redis-cli ping)
+#     if [[ "$pong" == *"PONG"* ]]; then
+#         echo ok;
+#         break
+#     fi
+# done
+
+# if [[ $env === 'production']]; then
+#     kubectl create -f k8s/production/deployment.yaml
+# else
+#     kubectl create -f k8s/staging/deployment.yaml
+# fi
+
+# echo "done"
+# echo "exit 0"
