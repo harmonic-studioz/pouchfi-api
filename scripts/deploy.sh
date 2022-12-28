2e2567a8f3d2c0f82f736d4f3286d8d56494f0e1
Daniel Chiwuzoh <chiwuzohdumebi@gmail.com>
2022-12-28 11:35:10 +0100

    update action

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -29,20 +29,11 @@ sed -i 's|thedumebi/pouchfi:latest|'$image_tag'|gi' ./k8s/$env/deployment.yaml
 echo "Save DigitalOcean kubeconfig with short-lived credentials"
 doctl kubernetes cluster kubeconfig save --expiry-seconds 600 $cluster_name
 
-kubectl get pods
+pod_name=$(kubectl get po -l app=pouchfi-redis | grep pouchfi-backend | awk '{print $1}')
+echo "pod_name: $pod_name"
 
-# kubectl apply -f staging
 
-# # run this once
-# kubectl create configmap pouchfi-env --from-env-file=./.env.staging
-# kubectl apply -f k8s/secret.yaml
-# kubectl create -f k8s/staging/pv.yaml
-
-# kubectl create -f k8s/staging/redis.yaml -f k8s/staging/postgres.yaml
-
-# pod_name=$(kubectl get po -l app=pouchfi-redis | grep pouchfi-backend | awk '{print $1}')
-
-# # check whether redis server is ready or not
+# check whether redis server is ready or not
 # while true; do
 #     pong=$(kubectl exec -it $pod_name redis -- redis-cli ping)
 #     if [[ "$pong" == *"PONG"* ]]; then
@@ -51,11 +42,8 @@ kubectl get pods
 #     fi
 # done
 
-# if [[ $env === 'production']]; then
-#     kubectl create -f k8s/production/deployment.yaml
-# else
-#     kubectl create -f k8s/staging/deployment.yaml
-# fi
+echo "Apply backend yaml"
+# kubectl apply -f ./k8s/$env/deployment.yaml
 
 echo "done"
 echo "exit 0"
